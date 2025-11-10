84772a6afe6ec4c7f75b17b56c782df80d25160c
Mohit Bisht <bishtmohit2023@gmail.com>
2025-11-10 22:38:21 +0530

    Register user controller added

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,42 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password, role, bio } = req.body;
+  if ([email, username, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const existedUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const user = await User.create({
+    username,
+    avatar: avatar.url,
+    email,
+    password,
+    role,
+    bio,
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the User");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+});
+
+export { registerUser };
