26d11a585fcc0586dc135c7e8b8dfc038f4caa03
Mohit Bisht <bishtmohit2023@gmail.com>
2025-11-12 09:19:14 +0530

    route updated

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { registerUser, loggedInUser } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loggedInUser,
+  logoutUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -9,4 +15,9 @@ router
   .post(upload.fields([{ name: "avatar", maxCount: 1 }]), registerUser);
 
 router.route("/login").post(loggedInUser);
+
+//secure routes
+
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 export default router;
