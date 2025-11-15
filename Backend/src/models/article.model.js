e1bafb357f634e8c1e6b18a038777dfa563b7da0
Mohit Bisht <bishtmohit2023@gmail.com>
2025-11-15 23:35:59 +0530

    Article model added

---
+++ Backend/src/models/article.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  content: {
+    type: String,
+    required: true,
+  },
+
+  tags: [
+    {
+      type: String,
+    },
+  ],
+
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  views: {
+    type: Number,
+    default: 0,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Article = mongoose.model("Article", articleSchema);
+export default Article;
