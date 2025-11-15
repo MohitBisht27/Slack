fc1d23a020ccae96f94ef674023fb79852a86152
Mohit Bisht <bishtmohit2023@gmail.com>
2025-11-15 23:36:57 +0530

    model updated

--- Backend/src/models/article.model.js
+++ Backend/src/models/article.model.js
@@ -1,51 +1,44 @@
 import mongoose from "mongoose";
 
-const articleSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  content: {
-    type: String,
-    required: true,
-  },
-
-  tags: [
-    {
+const articleSchema = new mongoose.Schema(
+  {
+    title: {
       type: String,
+      required: true,
+      trim: true,
     },
-  ],
 
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+    content: {
+      type: String,
+      required: true,
+    },
 
-  likes: [
-    {
+    tags: [
+      {
+        type: String,
+      },
+    ],
+
+    author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
-  ],
 
-  views: {
-    type: Number,
-    default: 0,
-  },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    views: {
+      type: Number,
+      default: 0,
+    },
   },
-
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 const Article = mongoose.model("Article", articleSchema);
 export default Article;
