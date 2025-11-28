1bb06c9070bc1d73b9a9462325967ec42202ea49
Mohit Bisht <bishtmohit2023@gmail.com>
2025-11-28 23:49:46 +0530

    Add Layout, Header, and Footer components with routing setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,29 @@
 import AskProblem from "./components/Editor/Problem";
 import SigninForm from "./components/SignIn/SignIn";
 import RegisterForm from "./components/SignUp/SignUp";
+import {
+  Router,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Layout from "./Layout";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<AskProblem />}></Route>
+      <Route path="/SigninForm" element={<SigninForm></SigninForm>}></Route>
+      <Route
+        path="/RegisterForm"
+        element={<RegisterForm></RegisterForm>}
+      ></Route>
+    </Route>
+  )
+);
 function App() {
-  return (
-    <div>
-      {/* <RegisterForm></RegisterForm> */}
-      {/* <AskProblem></AskProblem> */}
-      <SigninForm></SigninForm>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
