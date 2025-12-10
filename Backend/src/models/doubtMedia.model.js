3e609ad6fe03209570019e9945b14e244106ac7a
Mohit Bisht <bishtmohit2023@gmail.com>
2025-12-10 09:10:41 +0530

    media model added

---
+++ Backend/src/models/doubtMedia.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const mediaSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    video: { type: String },
+    image: { type: String },
+    mediaType: {
+      type: String,
+      enum: ["image", "video"],
+      required: true,
+    },
+    tags: [{ type: String, trim: true }],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    views: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+mediaSchema.index({ title: "text", tags: 1, user: 1 });
+
+const DoubtMedia = mongoose.model("DoubtMedia", mediaSchema);
+
+export default DoubtMedia;
