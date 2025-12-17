4a408313968a70f5106b23d0d1a5e64f4846af5e
Lakshya_Agarwal <149504941+Lakshya3704@users.noreply.github.com>
2025-12-17 09:16:18 +0530

    UI Landing page

---
+++ Frontend/src/pages/Landing.jsx
@@ -0,0 +1,22 @@
+import Navbar from './Component/Navbar'
+import Hero from './Component/Hero'
+import Process from './Component/Process'
+import About from './Component/About'
+import Service from './Component/Service'
+import Testimonial from './Component/Testimonial'
+import Contact from './Component/Contact'
+import Footer from './Component/Footer'
+function Landing() {
+    <div className='min-h-screen bg-gray-50'>
+        <Navbar />
+        <Hero />
+        <Process />
+        <About />
+        <Service />
+        <Testimonial />
+        <Contact />
+        <Footer />
+    </div>
+}
+
+export default Landing
